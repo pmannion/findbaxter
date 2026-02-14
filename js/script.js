2233581802970f4e53734f2dc69f1991899175b9
Paul Mannion <mannion.paul@gmail.com>
2026-02-14 10:28:49 +0000

    Refactor search popup and product quantity functions

--- js/script.js
+++ js/script.js
@@ -28,48 +28,7 @@
       })
     }
 
-    var countdownTimer = function() {
-      function getTimeRemaining(endtime) {
-        const total = Date.parse(endtime) - Date.parse(new Date());
-        const seconds = Math.floor((total / 1000) % 60);
-        const minutes = Math.floor((total / 1000 / 60) % 60);
-        const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-        const days = Math.floor(total / (1000 * 60 * 60 * 24));
-        return {
-          total,
-          days,
-          hours,
-          minutes,
-          seconds
-        };
-      }
-  
-      function initializeClock(id, endtime) {
-        const clock = document.getElementById(id);
-        const daysSpan = clock.querySelector('.days');
-        const hoursSpan = clock.querySelector('.hours');
-        const minutesSpan = clock.querySelector('.minutes');
-        const secondsSpan = clock.querySelector('.seconds');
-  
-        function updateClock() {
-          const t = getTimeRemaining(endtime);
-          daysSpan.innerHTML = t.days;
-          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-          if (t.total <= 0) {
-            clearInterval(timeinterval);
-          }
-        }
-        updateClock();
-        const timeinterval = setInterval(updateClock, 1000);
-      }
-  
-      $('#countdown-clock').each(function(){
-        const deadline = new Date(Date.UTC(2026, 1, 14, 13, 0, 0));
-        initializeClock('countdown-clock', deadline);
-      });
-    }
+
 
     var initProductQty = function(){
 
@@ -173,5 +132,6 @@
       const preloader = document.getElementById("preloader");
       preloader.classList.add("hide-preloader");
     });
+
 
 })(jQuery);
